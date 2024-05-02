44389dc0ef57bb31a1722991a026e3a69bfc4b20
kirill790 <kirya.kiryushin.99@mail.ru>
2024-05-02 19:41:48 +0300

    Add files via upload

---
+++ kirill.py
@@ -0,0 +1,34 @@
+x = 38
+print('дратути')
+if x < 0:
+    print('меньше нуля')
+print('дотвидания!')
+ # примеры
+a , b = 10,5
+if ('a > b'):
+     print (a > b)
+if a >b and a>0:
+    print('успех')
+if (a >b) and (a>0 or b < 1000):
+    print('успех')
+if 5 < a and b < 10:
+    print('успех')
+
+#сравнение чисел строк
+if '34'> '123':
+    print('успех')
+
+if '123' > '12':
+        print('успех')
+
+if [1, 2] > [1, 1]:
+    print('yсnex')
+
+#if '6' > 2:
+    print ( 'усnex')
+#if [5, 6] > 5:
+    print ( 'усnex')
+# HO
+if '6' != 5:
+    print ( 'усnex')
+
